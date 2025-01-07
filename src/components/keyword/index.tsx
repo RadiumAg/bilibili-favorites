0792af1bb6b01d304b40c8a9e98fd50fa80bba4b
radium <2043869032@qq.com>
2025-01-07 10:27:34 +0800

    feat: add scroll area component and dependency

--- src/components/keyword/index.tsx
+++ src/components/keyword/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useEditKeyword } from '@/hooks'
 import classNames from 'classnames'
+import { ScrollArea } from '@/components/ui/scroll-area'
 
 type KeywordProps = React.PropsWithChildren<{
   className?: string
@@ -11,19 +12,16 @@ const Keyword: React.FC<KeywordProps> = (props) => {
   const { tagElementArray, handleKeyDown } = useEditKeyword()
 
   return (
-    <div
-      className={classNames(
-        'border-solid border-cyan-200 border-2 h-44  flex flex-wrap p-1 items-start content-start gap-1 overflow-auto rounded-sm',
-        className,
-      )}
-    >
-      {tagElementArray}
+    <ScrollArea className={classNames('h-44 border-solid border-cyan-200 border-2', className)}>
+      <div className="flex flex-wrap p-1 items-start content-start gap-1 overflow-auto rounded-sm">
+        {tagElementArray}
 
-      <input
-        className="outline-none bg-transparent p-1  text-b-text-primary"
-        onKeyDown={handleKeyDown}
-      />
-    </div>
+        <input
+          className="outline-none bg-transparent p-1  text-b-text-primary"
+          onKeyDown={handleKeyDown}
+        />
+      </div>
+    </ScrollArea>
   )
 }
 
