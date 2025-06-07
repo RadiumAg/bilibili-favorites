bc86e81efb691007b11b8b3297454c56da4fd5b9
Radium <2043869032@qq.com>
2025-06-07 23:06:23 +0800

    build: 更新 tsconfig.json 类型声明并添加 EventSource 支持

--- src/hooks/use-create-keyword-by-ai/index.tsx
+++ src/hooks/use-create-keyword-by-ai/index.tsx
@@ -26,7 +26,7 @@ const useCreateKeywordByAi = () => {
         dataProvideData.aiConfig.model!,
       )
       const render = gptResult.toReadableStream().getReader()
-
+     EventSource
       let result = ''
 
       while (true) {
