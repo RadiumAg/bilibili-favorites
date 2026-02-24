887a7e8416c6b8e7e21fd9dfc4e35760572bb7ec
zly <zly01858966@alibaba-inc.com>
2026-02-24 14:33:34 +0800

    fix: 修复 ZIP 文件名中文乱码问题

--- src/zip.js
+++ src/zip.js
@@ -2,9 +2,9 @@ import gulp from 'gulp'
 import zip from 'gulp-zip'
 import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
-const manifest = require('../build/manifest.json')
+const packageJson = require('../package.json')
 
 gulp
   .src('build/**')
-  .pipe(zip(`${manifest.name.replaceAll(' ', '-')}-${manifest.version}.zip`))
+  .pipe(zip(`${packageJson.name}-v${packageJson.version}.zip`))
   .pipe(gulp.dest('package'))
